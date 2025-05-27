154c83c3ed1c4717cd722bad9bb501ed391801b5
sebastian438 <sebastian07051999@gmail.com>
2025-05-27 10:45:37 +0200

    ADD: seudocódigo añadido

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//LLAMAR ELEMENTOS DEL DOM
+
+
+//VARIABLES
+
+
+//EVENTOS
+
+
+//FUNCIONES
+
+const llamarApi = () => {
+    // Solicitud a la api con fetch (header autorizacion)
+    // Recoger objeto json
+    // try (devuelve todobien) catch (devuelve error) --> se pasa a pintar en evento
+}
+
+const validacion = () => {
+    //validar palabra introducida por input(con regular expresions)
+    //Letras mayúsculas, mínusculas, tíldes.
+}
+
+
+
+const filtrarBusqueda = () => {
+    //Llamamos a funcion validar, le pasamos la palabra, si es correcta, agregamos esa palabra en la URL de la api. Llamaríamos a funcion pintarFotos.
+}
+
+
+const filtrarOrientacion = () => {
+    //Llamamos a funcion Api, le pasamos la URL con la orientación seleccionada. Llamamos a funcion pintarFotos.
+}
+
+
+const aniadirAfavoritos = () => {
+    //Capturar la URL de la foto seleccionada. Guardar en LocalStorage. 
+}
+
+const getFavoritos = () => {
+    //Recogemos fotos guardadas en localStorage. 
+}
+
+const pintarFotosFavoritos = () => {
+    // Recoger el array del localstorage
+    // pintar foto + botón eliminar
+}
+
+const eliminarFoto = () => {
+    // Modificar array, si el id de la foto coincide con el id del botón eliminar, se filtra y no se añade
+}
+
+const pintarFotos = () => {
+    //Acceder al DOM, crear los elementos, acceder a la URL de las fotos y pintarlas 
+    // Enlazar botones con prev_page y next_page
+}
+
+//INVOCAR FUNCIONES
