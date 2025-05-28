25f3dd15294b6e5caa50f8d9df57cc88e5444f15
sebastian438 <sebastian07051999@gmail.com>
2025-05-28 11:33:12 +0200

    ADD: funcion pintar añadida

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //LLAMAR ELEMENTOS DEL DOM
-
+const sectionResultados = document.querySelector("#section-resultados");
+const sectionImagenes = document.querySelector("#section-imagenes");
+const fragment = document.createDocumentFragment();
 
 //VARIABLES
 const urlApiBase = "https://api.pexels.com/v1";
 const keyApi = "Oh6U5BGqs7r2Tfa2fTErGZUPAZA0XeC6z1iNLtx6Aiq1S9GiWJ3F8fpc";
 
-
 //EVENTOS
-
 
 //FUNCIONES
 /**
@@ -36,34 +36,87 @@ const llamarApi = async (endpoint) => {
     }
 }
 
-llamarApi("search?query=people");
+const pintarFotos = async () => {
+    try {
 
-const validacion = () => {
-    // const RegExp = / /[a-zA-ZÀ-ÿ\s]//;
-    //const imagen = (photoinput.valu);
-    // var mifotos;
-    /* if (imagen.search(RegExp) != -1) {
-      mifotos= " contains ";
-    } else {
-      mifotos = " does not contain ";
+        const data = await llamarApi("search?query=people");
+        console.log(data);
+
+        data.photos.forEach(element => {
+            const articleImagen = document.createElement("ARTICLE");
+            articleImagen.classList.add("imagen-card");
+
+            const imgContainer = document.createElement("DIV");
+            imgContainer.classList.add("imgContainer");
+
+            const imagen = document.createElement("IMG");
+            imagen.setAttribute("src", element.src.medium);
+            imagen.setAttribute("alt", element.alt);
+
+            const parrafoAutor = document.createElement("P");
+            parrafoAutor.textContent = element.photographer;
+
+            const btnFavoritosAdd = document.createElement("BUTTON");
+            btnFavoritosAdd.classList.add("btnFavoritosAdd");
+            btnFavoritosAdd.textContent = "Agregar a favoritos";
+
+            imgContainer.append(imagen);
+            articleImagen.append(imgContainer);
+            articleImagen.append(parrafoAutor);
+            articleImagen.append(btnFavoritosAdd);
+            fragment.append(articleImagen);
+
+
+        });
+        sectionImagenes.append(fragment);
+        const boxBtnPaginacion = document.createElement("ARTICLE");
+        boxBtnPaginacion.classList.add("btn-paginacion");
+        boxBtnPaginacion.classList.add("display-flex");
+
+        const prevPagBtn = document.createElement("BUTTON");
+        prevPagBtn.setAttribute("id", "prevPage");
+        prevPagBtn.textContent = "<<";
+
+        const paginaActualBtn = document.createElement("BUTTON");
+        paginaActualBtn.setAttribute("id", "currentPage");
+        paginaActualBtn.textContent = data.page;
+
+        const nextPagBtn = document.createElement("BUTTON");
+        nextPagBtn.setAttribute("id", "prevPage");
+        nextPagBtn.textContent = ">>";
+
+        boxBtnPaginacion.append(prevPagBtn);
+        boxBtnPaginacion.append(paginaActualBtn);
+        boxBtnPaginacion.append(nextPagBtn);
+        fragment.append(boxBtnPaginacion);
+        sectionResultados.append(fragment);
+
+
+    } catch (error) {
+        const parrafoErrorImage = document.createElement("P");
+        parrafoErrorImage.textContent = error;
+
+        sectionResultados.append(parrafoErrorImage);
     }
-    console.log(imagen + midstring + RegExp); */
+
+    //Acceder al DOM, crear los elementos, acceder a la URL de las fotos y pintarlas 
+    // Enlazar botones con prev_page y next_page
+}
+
+const validacion = (valida) => {
+    const regExp = /^[a-zA-Z\s]{3,}$/;
+    return regExp.test(valida);
     //validar palabra introducida por input(con regular expresions)
     //Letras mayúsculas, mínusculas, tíldes.
 }
 
-
-
 const filtrarBusqueda = () => {
-
     //Llamamos a funcion validar, le pasamos la palabra, si es correcta, agregamos esa palabra en la URL de la api. Llamaríamos a funcion pintarFotos.
 }
-
 
 const filtrarOrientacion = () => {
     //Llamamos a funcion Api, le pasamos la URL con la orientación seleccionada. Llamamos a funcion pintarFotos.
 }
-
 
 const aniadirAfavoritos = () => {
     //Capturar la URL de la foto seleccionada. Guardar en LocalStorage. 
@@ -78,13 +131,11 @@ const pintarFotosFavoritos = () => {
     // pintar foto + botón eliminar
 }
 
-const eliminarFoto = () => {
+const eliminarFotoFavoritos = () => {
     // Modificar array, si el id de la foto coincide con el id del botón eliminar, se filtra y no se añade
 }
 
-const pintarFotos = () => {
-    //Acceder al DOM, crear los elementos, acceder a la URL de las fotos y pintarlas 
-    // Enlazar botones con prev_page y next_page
-}
-
 //INVOCAR FUNCIONES
+
+// llamarApi("search?query=people");
+pintarFotos();
